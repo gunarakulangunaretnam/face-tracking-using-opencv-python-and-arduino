b19ca5fb6c37068965adbed053d01b132675058b
GunaRakulan-GR <gunarakulan@gmail.com>
2021-12-02 15:36:34 +0530

    Develop the basic face detection with cvzone

--- face-detection.py
+++ face-detection.py
@@ -1,34 +1,15 @@
 import cv2
-import numpy as np
-import time
 from cvzone.FaceDetectionModule import FaceDetector
 
-videoCam = cv2.VideoCapture(3)
-
+cap = cv2.VideoCapture(3)
 detector = FaceDetector()
 
 while True:
     success, img = cap.read()
-    img, bboxs = detector.findFaces(img, draw=False)
-
-    fx, fy = bboxs[0]["center"][0], bboxs[0]["center"][1]
-    print(fx)
-    print(fy)
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-videoCam.release()
+    img, bboxs = detector.findFaces(img) #draw=False will not draw the bounding box
+        
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
 cv2.destroyAllWindows()
