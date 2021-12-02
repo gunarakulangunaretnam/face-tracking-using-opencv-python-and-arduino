57efaf8db384166304dc48c169b76ec38e56c225
GunaRakulan-GR <gunarakulan@gmail.com>
2021-12-02 15:45:58 +0530

    Finish forehead pointer

---
+++ forhead-aimmer.py
@@ -0,0 +1,39 @@
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+import pyfirmata
+import numpy as np
+
+cap = cv2.VideoCapture(3)
+ws, hs = 1280, 720
+cap.set(3, ws)
+cap.set(4, hs)
+
+detector = FaceDetector()
+
+while True:
+    success, img = cap.read()
+    img, bboxs = detector.findFaces(img, draw=False)
+
+    if bboxs:
+        #get the coordinate
+        fx, fy = bboxs[0]["center"][0], bboxs[0]["center"][1]
+        fy  = fy - 90
+       
+        pos = [fx, fy]
+
+        cv2.circle(img, (fx, fy), 80, (0, 0, 255), 2)
+        cv2.putText(img, str(pos), (fx+15, fy-15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2 )
+        cv2.line(img, (0, fy), (ws, fy), (0, 0, 0), 2)  # x line
+        cv2.line(img, (fx, hs), (fx, 0), (0, 0, 0), 2)  # y line
+        cv2.circle(img, (fx, fy), 15, (0, 0, 255), cv2.FILLED)
+        cv2.putText(img, "TARGET LOCKED", (850, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3 )
+
+    else:
+        cv2.putText(img, "NO TARGET", (880, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
+        cv2.circle(img, (640, 360), 80, (0, 0, 255), 2)
+        cv2.circle(img, (640, 360), 15, (0, 0, 255), cv2.FILLED)
+        cv2.line(img, (0, 360), (ws, 360), (0, 0, 0), 2)  # x line
+        cv2.line(img, (640, hs), (640, 0), (0, 0, 0), 2)  # y line
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
