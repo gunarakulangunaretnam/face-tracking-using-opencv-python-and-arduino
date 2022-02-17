8af4ad294dec95bdcb124df2d86ea2034da84b14
GunaRakulan-GR <gunarakulan@gmail.com>
2022-02-17 20:08:32 +0530

    Cleanup

--- 0-forhead-aimmer-detection-only.py
+++ 0-forhead-aimmer-detection-only.py
@@ -3,7 +3,7 @@ from cvzone.FaceDetectionModule import FaceDetector
 import pyfirmata
 import numpy as np
 
-cap = cv2.VideoCapture(3)
+cap = cv2.VideoCapture(0)
 ws, hs = 1280, 720
 cap.set(3, ws)
 cap.set(4, hs)
@@ -22,11 +22,11 @@ while True:
         pos = [fx, fy]
 
         cv2.circle(img, (fx, fy), 80, (0, 0, 255), 2)
-        cv2.putText(img, str(pos), (fx+15, fy-15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2 )
+        cv2.putText(img, str(pos), (fx+15, fy-15), cv2.FONT_HERSHEY_PLAIN, 2, (13, 176, 217), 2 )
         cv2.line(img, (0, fy), (ws, fy), (0, 0, 0), 2)  # x line
         cv2.line(img, (fx, hs), (fx, 0), (0, 0, 0), 2)  # y line
         cv2.circle(img, (fx, fy), 15, (0, 0, 255), cv2.FILLED)
-        cv2.putText(img, "TARGET LOCKED", (850, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3 )
+        cv2.putText(img, "TARGET LOCKED", (850, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3 )
 
     else:
         cv2.putText(img, "NO TARGET", (880, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
